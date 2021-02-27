dabefb39d8a07e1041f9fab43d96c5fca86a7244
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2021-02-27 18:29:40 +0000

    Change functions to SHA-512 definitions

--- src/sha512.h
+++ src/sha512.h
@@ -3,23 +3,23 @@
 
 #include <inttypes.h>
 
-#define WORD_SIZE       32
-#define WORD            uint32_t
-#define PF              PRIX32
+#define WORD_SIZE       64
+#define PF              PRIX64
 
 #define Ch(x, y, z)     ((x) & (y)) ^ (~(x) & (z))
-#define Maj(x, y, z)    ((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z))
+#define Maj(x, y, z)    ((x) & (y)) ^ ( (x) & (z)) ^ ((y) & (z))
 
 #define SHR(x, n)       (x) >> (n)
 #define ROTR(x, n)      SHR((x), (n)) | ((x) << (WORD_SIZE - (n)))
+#define ROTL(x, n)      ((x) <<  (n)) | ((x) >> (WORD_SIZE - (n)))
 
-#define UP_SIG_0(x)     ROTR((x), 2) ^ ROTR((x), 13) ^ ROTR((x), 22)
-#define UP_SIG_1(x)     ROTR((x), 6) ^ ROTR((x), 11) ^ ROTR((x), 25)
+#define CAP_SIG_0(x)    ROTR((x), 28) ^ ROTR((x), 34) ^ ROTR((x), 39)
+#define CAP_SIG_1(x)    ROTR((x), 14) ^ ROTR((x), 18) ^ ROTR((x), 41)
+#define LOW_SIG_0(x)    ROTR((x),  1) ^ ROTR((x),  8) ^  SHR((x),  7)
+#define LOW_SIG_1(x)    ROTR((x), 19) ^ ROTR((x), 61) ^  SHR((x),  6)
 
-#define LOW_SIG_0(x)    ROTR((x), 7) ^ ROTR((x), 18) ^ SHR((x), 3)    
-#define LOW_SIG_1(x)    ROTR((x), 17) ^ ROTR((x), 19) ^ SHR((x), 10)
+typedef uint64_t Word;
 
 char* compute(const char* filename);
-void bin_print(unsigned int i);
 
 #endif
